0621cefc1ab3bab416aef0d9aeb4fd826a8279cf
Viktor Berke <github.bviktor@outlook.com>
2017-12-21 21:36:51 +0100

    The frequently visited folders have changed

--- bin/02_kf2.sh
+++ bin/02_kf2.sh
@@ -6,6 +6,9 @@ set -e
 sudo -u steam sh -c '~/Steam/steamcmd.sh +login anonymous +force_install_dir ./KF2Server +app_update 232130 validate +exit'
 
 # easy access
-sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/Config ~/Config'
-sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/BrewedPC/Maps ~/Maps'
-sudo -u steam sh -c 'ln -s /var/www/public_html ~/RedirectServer'
+# we have ini merge and workshop subscription so these are obsolete now
+#sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/Config ~/Config'
+#sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/BrewedPC/Maps ~/Maps'
+#sudo -u steam sh -c 'ln -s /var/www/public_html ~/RedirectServer'
+sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/Cache ~/Cache'
+sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/Binaries/Win64/steamapps/workshop ~/Workshop'
