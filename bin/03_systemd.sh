7182582134a0357a95fff256aa7e9d86b2dd6cfc
Viktor Berke <github.bviktor@outlook.com>
2019-01-06 18:24:18 +0100

    systemd

--- bin/03_systemd.sh
+++ bin/03_systemd.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-set -e
+set -eu
+
+echo -n 'Installing and enabling KF2 services... '
 
 # deploy the unit
-cp share/kf2.service /etc/systemd/system/
-cp share/kf2autokick.service /etc/systemd/system/
+cp -f share/kf2.service /etc/systemd/system/
+cp -f share/kf2autokick.service /etc/systemd/system/
 mkdir -p /etc/systemd/system/kf2.service.d
 ln -sf ${STEAM_HOME}/Config/Startup.conf /etc/systemd/system/kf2.service.d/kf2.service.conf
 
 # fix selinux context
-restorecon -rv /etc/systemd/system
+restorecon -r /etc/systemd/system
 
 # systemd refresh
-systemctl daemon-reload
+systemctl --quiet daemon-reload
 
 # start it automatically
-systemctl enable kf2.service
-systemctl enable kf2autokick.service
+systemctl --quiet enable kf2.service
+systemctl --quiet enable kf2autokick.service
+
+echo 'done.'
