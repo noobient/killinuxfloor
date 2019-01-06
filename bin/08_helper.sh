2ec98adfda191fb4a48686b821dd1b634c3403c7
Viktor Berke <github.bviktor@outlook.com>
2019-01-06 18:59:23 +0100

    Helper

--- bin/08_helper.sh
+++ bin/08_helper.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
-set -e
+set -eu
+
+echo -n 'Installing helpers... '
 
 # create the dir
 sudo -u steam sh -c 'mkdir -p ~/bin'
 
-# make a local copy of kf2-centos for easier updates
-sudo -u steam sh -c "cd && git clone https://github.com/bviktor/kf2-centos.git"
+if [ -d ${STEAM_HOME}/kf2-centos ]
+then
+    # update
+    sudo -u steam sh -c 'cd ~/kf2-centos && git reset --quiet --hard && git pull --quiet'
+else
+    # make a local copy of kf2-centos for easier updates
+    sudo -u steam sh -c "cd && git clone --quiet https://github.com/bviktor/kf2-centos.git"
+fi
 
 # deploy the helpers
-ln -sT ${STEAM_HOME}/kf2-centos/share/kf2.sh ${STEAM_HOME}/bin/kf2.sh
-ln -sT ${STEAM_HOME}/kf2-centos/share/autokick.sh ${STEAM_HOME}/bin/autokick.sh
+ln -sTf ${STEAM_HOME}/kf2-centos/share/kf2.sh ${STEAM_HOME}/bin/kf2.sh
+ln -sTf ${STEAM_HOME}/kf2-centos/share/autokick.sh ${STEAM_HOME}/bin/autokick.sh
 
 # fix ownership
 chown -R steam.steam ${STEAM_HOME}/bin
 
 # fix selinux context
-restorecon -rv ${STEAM_HOME}/bin
+restorecon -r ${STEAM_HOME}/bin
+
+echo 'done.'
