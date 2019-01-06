574ee00d1c17ea4961c682bb67044e977320b690
Viktor Berke <github.bviktor@outlook.com>
2019-01-06 16:57:04 +0100

    Wrong toggle

--- bin/01_steamcmd.sh
+++ bin/01_steamcmd.sh
@@ -5,18 +5,18 @@ set -eu
 # Steam user
 if [ $(id -u steam 2>/dev/null || echo -1) -eq -1 ]
 then
-    echo -e 'Creating dedicated steam user... '
+    echo -n 'Creating dedicated steam user... '
     useradd --user-group --create-home steam
     echo 'done.'
 fi
 
 # Deps
-echo -e 'Installing dependencies... '
+echo -n 'Installing dependencies... '
 yum -y -q install glibc.i686 libstdc++.i686 unzip dos2unix
 echo 'done.'
 
 # SteamCMD
-echo -e 'Installing Steam... '
+echo -n 'Installing Steam... '
 
 # create the main steam dir
 sudo -u steam sh -c 'mkdir -p ~/Steam'
