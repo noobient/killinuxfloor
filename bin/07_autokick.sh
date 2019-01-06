2011db25fd8576e8893a4bb1bb4fd2cdcee56f6c
Viktor Berke <github.bviktor@outlook.com>
2019-01-06 18:44:51 +0100

    Autokick

--- bin/07_autokick.sh
+++ bin/07_autokick.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
-set -e
+set -eu
+
+echo -n 'Installing auto-kick bot... '
 
 # install the repos
 curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 
 # install the packages
-yum -y install nodejs yarn
+yum -y -q install nodejs yarn
 
-# obtain a copy of kf2_autokick
-# wonderful workaround for "could not change back" bullcrap by git
-sudo -u steam sh -c "cd && git clone https://github.com/Sinewyk/kf2_autokick.git kf2autokick"
+if [ -d ${STEAM_HOME}/kf2autokick ]
+then
+    sudo -u steam sh -c 'cd ~/kf2autokick && git reset --quiet --hard && git pull --quiet'
+else
+    # obtain a copy of kf2_autokick
+    # wonderful workaround for "could not change back" bullcrap by git
+    sudo -u steam sh -c "cd && git clone --quiet https://github.com/Sinewyk/kf2_autokick.git kf2autokick"
+fi
 
 # 'build' it
-sudo -u steam sh -c "cd ${STEAM_HOME}/kf2autokick && yarn --frozen-lockfile"
+sudo -u steam sh -c "cd ${STEAM_HOME}/kf2autokick && yarn --quiet --frozen-lockfile"
+
+echo 'done.'
