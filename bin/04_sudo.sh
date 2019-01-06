f3d759297181d845757447657218922ad8722f3c
Viktor Berke <github.bviktor@outlook.com>
2019-01-06 18:27:01 +0100

    Sudo

--- bin/04_sudo.sh
+++ bin/04_sudo.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-set -e
+set -eu
 
-# deploy the sudo role
-cp share/kf2-sudo /etc/sudoers.d/
-cp share/kf2autokick-sudo /etc/sudoers.d/
+echo -n 'Installing KF2 service delegation rules... '
 
-# fix selinux context
-restorecon -rv /etc/sudoers.d
+# sudo roles
+cp -f share/kf2-sudo /etc/sudoers.d/
+cp -f share/kf2autokick-sudo /etc/sudoers.d/
 
-# fix file permissions
+# selinux context
+restorecon -r /etc/sudoers.d
+
+# file permissions
 chmod 0440 /etc/sudoers.d/kf2-sudo
 chmod 0440 /etc/sudoers.d/kf2autokick-sudo
+
+echo 'done.'
