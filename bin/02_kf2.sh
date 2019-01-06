49cb4261e2b8427391c3879061c45299b8d7f4bb
Viktor Berke <github.bviktor@outlook.com>
2019-01-06 18:01:40 +0100

    Attempt to check install state

--- bin/02_kf2.sh
+++ bin/02_kf2.sh
@@ -1,14 +1,38 @@
 #!/bin/sh
 
-set -e
+set -Eeu
+
+function errorexit ()
+{
+    case $? in
+        1)
+            echo 'error! Killing Floor 2 failed to install.'
+            echo 'Observe the SteamCMD output and check the files under /home/steam/Steam/logs.'
+            echo 'Once you eliminated the problem, try running the installer again.'
+            ;;
+
+        *)
+            echo 'error!'
+            ;;
+
+    esac
+}
+
+trap errorexit EXIT
+
+echo 'Installing Killing Floor 2... '
 
 # acquire the kf2 binaries
 sudo -u steam sh -c '~/Steam/steamcmd.sh +login anonymous +force_install_dir ./KF2Server +app_update 232130 validate +exit'
+echo 'Killing Floor 2 installer exited.'
+
+echo -n 'Checking Killing Floor 2 install state... '
+export STATE_LOG="${STEAM_HOME}/Steam/logs/state.log"
+sudo -u steam sh -c "~/Steam/steamcmd.sh +login anonymous +force_install_dir ./KF2Server +app_status 232130 validate +exit > ${STATE_LOG}"
+sudo -u steam sh -c "grep -i 'install state:' ${STATE_LOG} | grep -i 'fully installed' || exit 1"
 
 # easy access
-# we have ini merge and workshop subscription so these are obsolete now
-#sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/Config ~/Config'
-#sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/BrewedPC/Maps ~/Maps'
-#sudo -u steam sh -c 'ln -s /var/www/public_html ~/RedirectServer'
-sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/Cache ~/Cache'
-sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/Binaries/Win64/steamapps/workshop ~/Workshop'
+sudo -u steam sh -c 'ln -sf ~/Steam/KF2Server/KFGame/Cache ~/Cache'
+sudo -u steam sh -c 'ln -sf ~/Steam/KF2Server/Binaries/Win64/steamapps/workshop ~/Workshop'
+
+echo 'done.'
