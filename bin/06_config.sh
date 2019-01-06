9977ebd1927a0cea8b785b988f3489a58dc2b266
Viktor Berke <github.bviktor@outlook.com>
2019-01-06 18:31:09 +0100

    Crudini

--- bin/06_config.sh
+++ bin/06_config.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-set -e
+set -eu
+
+echo -n 'Installing config generator... '
 
 # install crudini
-yum -y install epel-release
-yum -y install crudini
+yum -y -q install epel-release
+yum -y -q install crudini
 
 # deploy the initial config
-cp -R share/Config ${STEAM_HOME}/
+cp -R -f share/Config ${STEAM_HOME}/
 
 # fix ownership
 chown -R steam.steam ${STEAM_HOME}/Config
 
 # fix selinux context
-restorecon -rv ${STEAM_HOME}/Config
+restorecon -r ${STEAM_HOME}/Config
 
 # easy access to stock config files
-sudo -u steam sh -c 'ln -s ~/Steam/KF2Server/KFGame/Config ~/Config/Internal'
+sudo -u steam sh -c 'ln -sfn ~/Steam/KF2Server/KFGame/Config ~/Config/Internal'
+
+echo 'done.'
