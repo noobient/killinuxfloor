7db0df9a9ea0ce19bedf02f9e8bdfee721bfc17c
Viktor Berke <github.bviktor@outlook.com>
2019-01-08 01:57:10 +0100

    Branding

--- share/kf2.sh
+++ share/kf2.sh
@@ -5,7 +5,7 @@ set -Eeu
 function print_help ()
 {
     #echo "Usage: kf2.sh {start|stop|restart|status|update [preview]|log|config|purge <map_id>|init|help}"
-    echo "Killing Floor 2 CentOS Admin Script (c) noobient"
+    echo "killinuxfloor - Killing Floor 2 Linux Server Installer and Manager (c) noobient"
     echo ""
     echo "Usage:"
     echo "------"
