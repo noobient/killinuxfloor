cbffa673228030e99c52d6f2f86aeca7493695e3
Viktor Berke <github.bviktor@outlook.com>
2022-12-21 01:57:47 +0100

    Add tester script

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -u
+
+if [ $# -lt 1 ]
+then
+    platform="fedora:37"
+else
+    platform="${1}"
+fi
+
+echo -e "\nRunning tests on:\n - Platform: \e[36m${platform}\e[0m\n"
+
+# Fire up instance
+CONT_ID=$(docker run --rm -v $(pwd):/repo -v /sys/fs/cgroup:/sys/fs/cgroup:ro --tmpfs /tmp --tmpfs /run --privileged --detach "bviktor/ansible-systemd-${platform}")
+# Run the tests
+docker exec ${CONT_ID} /bin/bash -c "dnf -y install firewalld && systemctl start firewalld.service"
+docker exec ${CONT_ID} /bin/bash -c "./install.sh --extra-vars 'skip_kfgame=true' <<< y"
+docker exec ${CONT_ID} /bin/bash -c "./uninstall.sh <<< y"
+docker exec ${CONT_ID} /bin/bash -c "./install.sh --extra-vars 'skip_kfgame=true kf2_classic=true' <<< y"
+# Let us check stuff before exiting
+docker exec -it "${CONT_ID}" bash
+# Stop instance
+docker stop "${CONT_ID}"
