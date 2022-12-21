d7e5eed627801240297b1a8fb699392a4f3e335c
Viktor Berke <github.bviktor@outlook.com>
2022-12-21 03:57:15 +0100

    Update manual test too

--- test.sh
+++ test.sh
@@ -16,8 +16,8 @@ CONT_ID=$(docker run --rm -v $(pwd):/repo -v /sys/fs/cgroup:/sys/fs/cgroup:ro --
 # Determine package manager
 export PKG_MGR=$(docker exec ${CONT_ID} /bin/bash -c "apt --version &> /dev/null && echo 'apt' || echo 'dnf'")
 # Run the tests
-docker exec ${CONT_ID} /bin/bash -c "DEBIAN_FRONTEND=noninteractive ${PKG_MGR} -y -q install firewalld > /dev/null && systemctl start firewalld.service"
 docker exec ${CONT_ID} /bin/bash -c "./install.sh --extra-vars 'skip_kfgame=true' <<< y"
+docker exec ${CONT_ID} /bin/bash -c "DEBIAN_FRONTEND=noninteractive ${PKG_MGR} -y -q install firewalld > /dev/null && systemctl start firewalld.service"
 docker exec ${CONT_ID} /bin/bash -c "./uninstall.sh <<< y"
 docker exec ${CONT_ID} /bin/bash -c "./install.sh --extra-vars 'skip_kfgame=true kf2_classic=true' <<< y"
 # Let us check stuff before exiting
